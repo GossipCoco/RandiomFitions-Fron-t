0b37b26255d0eae42490789dc729127f556347f0
Coralie Honorin <gossipcoco@hotmail.fr>
2024-10-09 10:08:25 +0200

    dependecies install

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Vuex from 'vuex'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import './assets/css/Base.css'
+const app = createApp(App)
+app
+    .use(Vuex)
+    .use(VueAxios, axios)
+    .mount('#app')
